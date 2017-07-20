1a28c8ef9d7ea7374d337375016bc410863347ce
Ted Michael Celis <krazete@gmail.com>
2017-07-20 15:48:57 -0700

    added bs4, changed names, and automated yearterms detection

--- websoc.py
+++ websoc.py
@@ -1,91 +1,75 @@
 from google.appengine.api import urlfetch
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 # https://github.com/gumho/antplanner2/blob/b8501534787b6541f05626907b18bd273e8bc567/antplanner2/views.py
 # https://www.reg.uci.edu/perl/WebSoc?YearTerm=2016-92&Submit=TextResults&CourseCodes=50000-54000
 
-'''
-javascript:
-/* Gets YearTerms from the WebSoc menu. */
-console.log(
-    ["Fall", "Winter", "Spring", "Session 1", "Session 2", "10-wk", "COM"]
-    .map(function(t){
-        return Array.from(new Set(
-               Array.from(document.forms[1].YearTerm.options)
-               .filter(function(e){return e.innerHTML.includes(t)})
-               .map(function(e){return e.value.split("-")[1]})
-        ))[0]
-    })
-);
+URL = 'https://www.reg.uci.edu/perl/WebSoc'
 
-/* Manually loops through CourseCode blocks. */
-var yearterm = "2016-92";
-var block = 1000;
-var next = Number(location.search
-           .slice(location.search.lastIndexOf("=") + 1)
-           .split("-")[0]) + block;
-if(next + block > 100000){
-    next = 0;
-}
-location = "https://www.reg.uci.edu/perl/WebSoc?YearTerm=" + yearterm
-         + "&Submit=TextResults&CourseCodes=" + next + "-" + (next + block - 1);
+def get():
+    'Returns content from WebSoc.'
+    return urlfetch.fetch(URL).content
 
-/* Gets all fieldnames from the WebSoc menu. */
-Array.from(new Set(
-Array.from(document.forms[1].elements)
-.map(function(e){return e.name})))
-'''
+def post(yearterm, coursecodes=None):
+    'Posts a request to WebSoc and returns the response content.'
+    params = '?YearTerm=' + yearterm
+    if coursecodes:
+        params += '&Submit=TextResults&CourseCodes=' + coursecodes
+    return urlfetch.fetch(URL + params).content
 
-# WEBSOC MAP
+def detect_yearterms():
+    'Detects the current yearterms.'
+    year = datetime.now().year
+    content = get()
+    html = BeautifulSoup(content, 'html.parser')
+    yearterms_html = html.find('select').find_all('option')
+    yearterms = map(lambda e: e['value'], yearterms_html)
+    return filter(lambda e: int(e.split('-')[0]) == year, yearterms)
 
-class websoc:
-    def __init__(self):
-        return
+def generate_yearterms():
+    'Generates current yearterms.'
+    yearterms = detect_yearterms()
+    for yearterm in yearterms:
+        content = post(yearterm)
+        if 'Currently in week' in content:
+            yield yearterm
 
-def post(yt, cc=None):
-    'Requests data from WebSoc and returns the response.'
-    url = 'https://www.reg.uci.edu/perl/WebSoc?YearTerm=' + yt
-    if cc:
-        url += '&Submit=TextResults&CourseCodes=' + cc
-    fetched = urlfetch.fetch(url)
-    return fetched.content
-
-def yearterms():
-    'Generates YearTerms from the current year.'
-    y = str(datetime.now().year)
-    ts = ['92', '03', '14', '25', '76', '39', '51']
-    yts = [y + '-' + t for t in ts]
-    for yt in yts:
-        data = post(yt)
-        if 'Currently in week' in data or 'Instruction starts on' in data:
-            yield yt
-
-def coursecodes():
-    'Generates CourseCodes from 0 to 99999 in blocks of 2000.'
-    for n in range(50):
-        a = n * 1000
-        b = a + 999
+def generate_coursecodes(n):
+    'Generates coursecodes from 0 to 99999 in blocks of whole number n.'
+    blocks = 100000 / n
+    remainder = 100000 % n
+    for i in range(blocks):
+        a = i * n
+        b = a + n - 1
+        yield str(a) + '-' + str(b)
+    if remainder:
+        a = b + 1
+        b = a + remainder - 1
         yield str(a) + '-' + str(b)
 
-def main():
-    db = []
-    for yt in yearterms():
-        for cc in coursecodes():
-            data = post(yt, cc)
-            if 'No courses matched' not in data:
-                db.append(data)
-                break # Sample the first 1000.
-    return db
+from logging import info
 
-def parse(data):
-    d = []
-    
+def get_data():
+    data = []
+    yearterms = generate_yearterms()
+    for yearterm in yearterms:
+        info(yearterm)
+        coursecodes = generate_coursecodes(800)
+        for coursecode in coursecodes:
+            info(coursecode)
+            content = post(yearterm, coursecode)
+            if 'No courses matched' not in content:
+                data.append(content)
+    return data
+
+def parse_data(data):
+    database = []
     lines = data.split('\n')
     for line in lines:
         if line != '':
-            d.append(line)
+            database.append(line)
+    return database
 
-    return d
-
-def update_cache():
+def store():
     pass
