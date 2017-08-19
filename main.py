9d071a5fd401f38cc0ca257cfffa53af300df727
Ted Michael Celis <krazete@gmail.com>
2017-08-19 13:59:06 -0700

    fixed url handlers

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import webapp2
-import websoc
 
 class Index(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        content = websoc.get_data()
-        self.response.write(content)
+        self.response.write(open('index.html').read())
 
 class Other(webapp2.RequestHandler):
     def get(self, path):
-        self.response.headers['Content-Type'] = 'text/' + path.split('.')[-1]
+        self.response.headers['Content-Type'] = 'text/' + path.split('.')[-1] # greedy
         self.response.write(open(path).read())
 
 sitemap = [
