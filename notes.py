0144f6f4bc3c06b3c35070b9fbffeeb082de6118
Ted Michael Celis <krazete@gmail.com>
2017-08-19 13:59:33 -0700

    updated notes

--- notes.py
+++ notes.py
@@ -20,6 +20,12 @@
 |   Display the times when a class is |
 |   held for a chosen classroom.      |
 +-------------------------------------+
+janitors come invariably between 8:00 and 11:00
+doors lock at 9:20?
+some systems shut down at 11:50???
+some systems restart at 1:00
+painter/maintenance at 2:45?
+doors unlock at 7
 '''
 
 import SimpleHTTPServer
@@ -28,7 +34,7 @@ import json
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers.getheader('content-length'))        
+        content_length = int(self.headers.getheader('content-length'))
         body = self.rfile.read(content_length)
         try:
             result = json.loads(body, encoding='utf-8')
