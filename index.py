95c33f3b08df5a0a71bcd74f6b8f147d39ab6a4e
Ted Michael Celis <krazete@gmail.com>
2017-09-26 22:49:39 -0700

    database initialization works now

--- index.py
+++ index.py
@@ -1,12 +1,21 @@
+from scrape import scrape
 import webapp2
-import antndb
 
 class Index(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(open('index.html').read().replace('{WEBSOC_DATA}', '999999999'))
-        self.response.write('<div>' + str(antndb.get()) + '</div>')
 
-sitemap = [('/', Index)]
+class Scrape(webapp2.RequestHandler):
+    def post(self):
+        year = int(self.request.get('year', None))
+        self.response.write(year)
+        scrape(year)
+        self.response.write(' has been added to the database.')
+
+sitemap = [
+    ('/', Index),
+    ('/scrape', Scrape)
+]
 
 app = webapp2.WSGIApplication(sitemap, debug=True)
