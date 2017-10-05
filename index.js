71e8365a0d4748d9d6f1cd319f6c7871084a1cd7
Ted Michael Celis <krazete@gmail.com>
2017-10-05 13:24:45 -0700

    replaced DataTables with original code

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 /* Building Information */
 var geo = {
-	BS3:  {lat: 33.64532, long: -117.84608, name: "Biological Sciences III"},
-	DBH:  {lat: 33.64326, long: -117.84198, name: "Donald Bren Hall"},
-	EH:   {lat: 33.64376, long: -117.84101, name: "Engineering Hall"},
-	ELH:  {lat: 33.64447, long: -117.84071, name: "Engineering Lecture Hall"},
-	HG:   {lat: 33.64825, long: -117.84451, name: "Humanities Gateway"},
-	HH:   {lat: 33.64733, long: -117.84398, name: "Humanities Hall"},
-	HIB:  {lat: 33.64841, long: -117.84383, name: "Humanities Instructional Building"},
-	HICF: {lat: 33.64687, long: -117.84680, name: "Humanities Interim Classroom Facility"},
-	HSLH: {lat: 33.64559, long: -117.84466, name: "Howard Schneiderman Lecture Hall"},
-	IAB:  {lat: 33.64822, long: -117.84534, name: "Intercollegiate Athletics Building"},
-	ICF:  {lat: 33.64443, long: -117.83997, name: "Interim Classroom Facility"},
-	ICS:  {lat: 33.64428, long: -117.84180, name: "Information & Computer Science"},
-	LLIB: {lat: 33.64713, long: -117.84109, name: "Langson Library"},
-	MM:   {lat: 33.64933, long: -117.84445, name: "Music & Media Building"},
-	MPAA: {lat: 33.64707, long: -117.83695, name: "Multipurpose Academic & Administrative Building"},
-	MSTB: {lat: 33.64208, long: -117.84442, name: "Multipurpose Science & Technology Building"},
-	PCB:  {lat: 33.64451, long: -117.84281, name: "Parkview Classroom Building"},
-	PSCB: {lat: 33.64341, long: -117.84349, name: "Physical Sciences Classroom Building"},
-	PSLH: {lat: 33.64340, long: -117.84396, name: "Physical Sciences Lecture Hall"},
-	RH:   {lat: 33.64451, long: -117.84412, name: "Rowland Hall"},
-	SBSG: {lat: 33.64737, long: -117.83909, name: "Social & Behavioral Sciences Gateway"},
-	SE:   {lat: 33.64615, long: -117.83889, name: "Social Ecology I"},
-	SE2:  {lat: 33.64656, long: -117.83915, name: "Social Ecology II"},
-	SH:   {lat: 33.64621, long: -117.84484, name: "Steinhaus Hall"},
-	SSH:  {lat: 33.64623, long: -117.84007, name: "Social Science Hall"},
-	SSL:  {lat: 33.64590, long: -117.84000, name: "Social Science Lab"},
-	SSLH: {lat: 33.64723, long: -117.83973, name: "Social Science Lecture Hall"},
-	SSPA: {lat: 33.64694, long: -117.83955, name: "Social Science Plaza A"},
-	SST:  {lat: 33.64646, long: -117.84027, name: "Social Science Tower"},
-	SSTR: {lat: 33.64698, long: -117.84024, name: "Social Science Trailer"}
+	BS3:  {"latitude": 33.64532, "longitude": -117.84608, "distance": 0, "name": "Biological Sciences III"},
+	DBH:  {"latitude": 33.64326, "longitude": -117.84198, "distance": 0, "name": "Donald Bren Hall"},
+	EH:   {"latitude": 33.64376, "longitude": -117.84101, "distance": 0, "name": "Engineering Hall"},
+	ELH:  {"latitude": 33.64447, "longitude": -117.84071, "distance": 0, "name": "Engineering Lecture Hall"},
+	HG:   {"latitude": 33.64825, "longitude": -117.84451, "distance": 0, "name": "Humanities Gateway"},
+	HH:   {"latitude": 33.64733, "longitude": -117.84398, "distance": 0, "name": "Humanities Hall"},
+	HIB:  {"latitude": 33.64841, "longitude": -117.84383, "distance": 0, "name": "Humanities Instructional Building"},
+	HICF: {"latitude": 33.64687, "longitude": -117.84680, "distance": 0, "name": "Humanities Interim Classroom Facility"},
+	HSLH: {"latitude": 33.64559, "longitude": -117.84466, "distance": 0, "name": "Howard Schneiderman Lecture Hall"},
+	IAB:  {"latitude": 33.64822, "longitude": -117.84534, "distance": 0, "name": "Intercollegiate Athletics Building"},
+	ICF:  {"latitude": 33.64443, "longitude": -117.83997, "distance": 0, "name": "Interim Classroom Facility"},
+	ICS:  {"latitude": 33.64428, "longitude": -117.84180, "distance": 0, "name": "Information & Computer Science"},
+	LLIB: {"latitude": 33.64713, "longitude": -117.84109, "distance": 0, "name": "Langson Library"},
+	MM:   {"latitude": 33.64933, "longitude": -117.84445, "distance": 0, "name": "Music & Media Building"},
+	MPAA: {"latitude": 33.64707, "longitude": -117.83695, "distance": 0, "name": "Multipurpose Academic & Administrative Building"},
+	MSTB: {"latitude": 33.64208, "longitude": -117.84442, "distance": 0, "name": "Multipurpose Science & Technology Building"},
+	PCB:  {"latitude": 33.64451, "longitude": -117.84281, "distance": 0, "name": "Parkview Classroom Building"},
+	PSCB: {"latitude": 33.64341, "longitude": -117.84349, "distance": 0, "name": "Physical Sciences Classroom Building"},
+	PSLH: {"latitude": 33.64340, "longitude": -117.84396, "distance": 0, "name": "Physical Sciences Lecture Hall"},
+	RH:   {"latitude": 33.64451, "longitude": -117.84412, "distance": 0, "name": "Rowland Hall"},
+	SBSG: {"latitude": 33.64737, "longitude": -117.83909, "distance": 0, "name": "Social & Behavioral Sciences Gateway"},
+	SE:   {"latitude": 33.64615, "longitude": -117.83889, "distance": 0, "name": "Social Ecology I"},
+	SE2:  {"latitude": 33.64656, "longitude": -117.83915, "distance": 0, "name": "Social Ecology II"},
+	SH:   {"latitude": 33.64621, "longitude": -117.84484, "distance": 0, "name": "Steinhaus Hall"},
+	SSH:  {"latitude": 33.64623, "longitude": -117.84007, "distance": 0, "name": "Social Science Hall"},
+	SSL:  {"latitude": 33.64590, "longitude": -117.84000, "distance": 0, "name": "Social Science Lab"},
+	SSLH: {"latitude": 33.64723, "longitude": -117.83973, "distance": 0, "name": "Social Science Lecture Hall"},
+	SSPA: {"latitude": 33.64694, "longitude": -117.83955, "distance": 0, "name": "Social Science Plaza A"},
+	SST:  {"latitude": 33.64646, "longitude": -117.84027, "distance": 0, "name": "Social Science Tower"},
+	SSTR: {"latitude": 33.64698, "longitude": -117.84024, "distance": 0, "name": "Social Science Trailer"}
 };
 
 function forGeo(f) {
@@ -44,30 +44,154 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 	maxZoom: 18,
 	id: "mapbox.streets"
 }).addTo(leafletmap);
-forGeo(function (e, o) {
-	L.circle([e.lat, e.long]).bindPopup(e.name + " (" + o + ")").addTo(leafletmap);
+forGeo(function(e, o) {
+	//L.circle([e.latitude, e.longitude]).bindPopup(e.name + " (" + o + ")").addTo(leafletmap);
 });
 
 var bubble;
-leafletmap.locate({watch: true}).on("locationfound", function(e) {
-	if (bubble)
+var marker;
+
+function distance(v, w) {
+	return Math.sqrt(Math.pow(w.latitude - v.latitude, 2) + Math.pow(w.longitude - v.longitude, 2));
+}
+
+function onLeafletLocate(e) {
+	if (bubble) {
 		bubble.removeFrom(leafletmap);
+	}
 	bubble = L.circle([e.latitude, e.longitude], e.accuracy / 2, {
 		weight: 0,
-		fillColor: "#00ffff",
-		fillOpacity: 0.25
+		fillColor: "#bdf",
+		fillOpacity: 0.5
 	}).addTo(leafletmap).bringToBack();
-}).on("locationerror", function(e){
+
+	if (marker) {
+		marker.removeFrom(leafletmap);
+	}
+	marker = L.marker([e.latitude, e.longitude]).addTo(leafletmap);
+
+	forGeo(function(object, name) {
+		object.distance = distance(e, object) * 1000;
+		L.circle([object.latitude, object.longitude], 15, {weight: 1, fillOpacity: 1 / Math.pow(Math.E, distance(e, object) * 500)}).bindPopup(object.name + " (" + name + ")").addTo(leafletmap);
+	});
+}
+
+function onLeafletLocateError(e) {
 	console.log(e.message);
-});
+}
+
+var watcher = leafletmap.locate({watch: true});
+watcher.on("locationfound", onLeafletLocate);
+watcher.on("locationerror", onLeafletLocateError);
 
 /* Table */
-forGeo(function (e, k) {
-	var td, tr = document.getElementById("listbox").getElementsByTagName("tbody")[0].insertRow();
-	td = tr.insertCell();
-	td.innerHTML = e.name;
-	td = tr.insertCell();
-	td.innerHTML = k;
-	td = tr.insertCell();
-	td.innerHTML = e.long;
-});
+
+var TimeTable = {
+	"new": function(data) {
+		var table = document.createElement("table");
+		init(table, data, properties);
+		return {
+			"data": data,
+			"table": table,
+			"sort": function() {
+				sortByDistance();
+				sortByHoursUntilNext();
+				sortByHoursFromLast();
+				sortByBuildingAndRoom();
+			},
+			"resize": function() {},
+			"changeDay": function(e) {
+				console.log(e.target.value);
+				for (var i = 1; i < rows.length; i++) {
+					rows[i];
+				}
+			}
+		};
+	},
+	"init": function(table, data, properties) {
+		TimeTable.initTHead(table, properties);
+		TimeTable.initTBody(table, data, properties);
+		TimeTable.initTFoot(table);
+		window.addEventListener("resize", table.resize);
+	},
+	"initTHead": function(table, properties) {
+		var thead = table.createTHead();
+		var tr = thead.insertRow();
+		var th = tr.appendChild(document.createElement("th"));
+		th.innerHTML = properties[0];
+		TimeTable.newDays(tr, true);
+		for (var i = 1; i < properties.length; i++) {
+			var th = tr.appendChild(document.createElement("th"));
+			th.innerHTML = properties[i];
+		}
+	},
+	"initTBody": function(table, data, properties) {
+		var tbody = table.createTBody();
+		var start = 0;
+		var end = data.length;
+		for (var i = start; i < end; i++) {
+			var tr = tbody.insertRow();
+			var td;
+			td = tr.insertCell();
+			td.innerHTML = data[i].bldg + " " + data[i].room;
+			td = tr.insertCell();
+			td.setAttribute("colspan", 7);
+			var timeline = TimeTable.newTimeline(6, 24);
+			td.appendChild(timeline);
+			for (var j = 6 * 2; j < 24 * 2; j++) {
+				if (data[i].we.some(function(e) {return e[0] * 2 <= j && j < e[1] * 2})) {
+					timeline.children[j - 6 * 2].classList.add("active");
+				}
+			}
+			td = tr.insertCell();
+			try {
+				td.innerHTML = geo[data[i].bldg].distance;
+			} catch(e) {}
+			td = tr.insertCell();
+			td.innerHTML = data[i].last_active;
+		}
+	},
+	"initTFoot": function(table) {
+		var tfoot = table.createTFoot();
+		var td = tfoot.insertRow().insertCell();
+		td.setAttribute("colspan", 10);
+		td.innerHTML = "Switch Sorting Priority | Toggle Column Visibility | Reset Location | Reset Time | Previous | Next | Search";
+	},
+
+	"newDays": function(tr, abbreviate) {
+		var days = abbreviate ? ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] : ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		for (var i = 0; i < 7; i++) {
+			var th = tr.appendChild(document.createElement("th"));
+			th.className = "day";
+			th.innerHTML = days[i];
+			th.value = i;
+			th.addEventListener("click", TimeTable.changeDay);
+		}
+	},
+	"newTimeline": function(a, b) {
+		var timeline = document.createElement("div");
+		timeline.className = "timeline";
+		for (var i = a * 2; i < b * 2; i++) {
+			var timeblock = timeline.appendChild(document.createElement("div"));
+			timeblock.className = "timeblock";
+			if ((i + 1) % 2) {
+				timeblock.innerHTML = (i / 2 - 1) % 12 + 1;
+			}
+		}
+		return timeline;
+	}
+};
+
+var table = document.createElement("table");
+table.className = "timetable";
+TimeTable.init(table, database, ["room", "distance", "last_active"]);
+document.getElementById("list").appendChild(table);
+
+// Notes
+// The TimeTable object will take Data (list of objects) and a Table.
+// It will add a Row property to every Datum.
+// There will be a sort function that can sort the Data and refresh the list of Rows displayed in the table.
+// When a Row is hovered over or clicked, certain Data will be displayed.
+//
+// Questions
+// Should I link each row to a piece of data, or should I link data to each row? Or neither?
