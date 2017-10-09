bd2b311927599baec1c1d2a59cdbea28d3aa75c3
Ted Michael Celis <krazete@gmail.com>
2017-10-09 12:12:40 -0700

    changed variable names, added default method

--- chrono.py
+++ chrono.py
@@ -1,8 +1,8 @@
 class chronolist:
-    def __init__(self, l=[]):
+    def __init__(self, items=[]):
         self.list = []
-        for e in l:
-            self.add(e)
+        for item in items:
+            self.add(item)
     def __iter__(self):
         return iter(self.list)
     def __len__(self, other):
@@ -11,53 +11,55 @@ class chronolist:
         return repr(self.list)
     def __str__(self):
         return str(self.list)
-    # def index(self, o):
-    #     if isinstance(o, (int, float, long, complex)):
-    #         o = [o, o]
-    #     for i, e in enumerate(self.list):
-    #         if e[0] <= o[0] <= o[1] <= e[1]:
+    def default(self):
+        return self.list
+    # def index(self, new_item):
+    #     if isinstance(new_item, (int, float, long, complex)):
+    #         new_item = [new_item, new_item]
+    #     for i, item in enumerate(self.list):
+    #         if item[0] <= new_item[0] <= new_item[1] <= item[1]:
     #             return i
     #     return -1
-    def add(self, o):
-        assert len(o) == 2, '{} is not a range'.format(o)
-        assert o[0] <= o[1], '{} is not a proper range'.format(o)
+    def add(self, new_item):
+        assert len(new_item) == 2, '{} is not a range'.format(o)
+        assert new_item[0] <= new_item[1], '{} is not a proper range'.format(o)
         # sorted insert
         i = 0
-        for e in self.list:
-            if o < e:
+        for item in self.list:
+            if new_item < item:
                 break
             i += 1
-        self.list.insert(i, o)
+        self.list.insert(i, new_item)
         if len(self.list) == 1:
             return
         # merge remaining
         cutlist = []
         if i >= 1:
-            e = self.list[i - 1]
-            if o[0] <= e[1]: # check behind
-                self.list[i][0] = min(e[0], o[0])
-                cutlist.append(e)
-        for e in self.list[i + 1:]: # check in front
-            if e[0] <= o[1]:
-                self.list[i][1] = max(e[1], o[1])
-                cutlist.append(e)
+            item = self.list[i - 1]
+            if new_item[0] <= item[1]: # check behind
+                self.list[i][0] = min(item[0], new_item[0])
+                cutlist.append(item)
+        for item in self.list[i + 1:]: # check in front
+            if e[0] <= new_item[1]:
+                self.list[i][1] = max(item[1], new_item[1])
+                cutlist.append(item)
             else:
                 break
-        for e in cutlist:
-            self.list.remove(e)
-    def remove(self, o):
-        assert len(o) == 2, '{} is not a range'.format(o)
-        assert o[0] < o[1], '{} is not a proper range'.format(o)
+        for item in cutlist:
+            self.list.remove(item)
+    def remove(self, new_item):
+        assert len(new_item) == 2, '{} is not a range'.format(o)
+        assert new_item[0] < new_item[1], '{} is not a proper range'.format(o)
         cutlist = []
-        for i, e in enumerate(self.list):
-            if o[0] <= e[0] <= o[1] <= e[1]: # o <= e
-                self.list[i][0] = o[1]
-            if e[0] <= o[0] <= e[1] <= o[1]: # e <= o
-                self.list[i][1] = o[0]
-            if e[0] <= o[0] <= o[1] <= e[1]: # e surrounds o
-                self.list.insert(i, [e[0], o[0]])
-                self.list[i + 1][0] = o[0]
-            if o[0] <= e[0] <= e[1] <= o[1]: # o surrounds e
-                cutlist.append(e)
-        for e in cutlist:
-            self.list.remove(e)
+        for i, item in enumerate(self.list):
+            if new_item[0] <= item[0] <= new_item[1] <= item[1]: # new_item <= item
+                self.list[i][0] = new_item[1]
+            if item[0] <= new_item[0] <= item[1] <= new_item[1]: # item <= new_item
+                self.list[i][1] = new_item[0]
+            if item[0] <= new_item[0] <= new_item[1] <= item[1]: # item surrounds new_item
+                self.list.insert(i, [item[0], new_item[0]])
+                self.list[i + 1][0] = new_item[0]
+            if new_item[0] <= item[0] <= item[1] <= new_item[1]: # new_item surrounds item
+                cutlist.append(item)
+        for item in cutlist:
+            self.list.remove(item)
