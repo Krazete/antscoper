02fc3138aec86e4dbba6297ff6059d329612c4c9
Ted Michael Celis <krazete@gmail.com>
2017-11-30 12:19:33 -0800

    renamed vars and put hotlinks

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 /* Building Information */
 // var geo = {
-// 	BS3:  {"latlng": L.latLng(33.64532, -117.84608), "distance": Infinity, "bubble": null, "name": "Biological Sciences III"},
-// 	DBH:  {"latlng": L.latLng(33.64326, -117.84198), "distance": Infinity, "bubble": null, "name": "Donald Bren Hall"},
-// 	EH:   {"latlng": L.latLng(33.64376, -117.84101), "distance": Infinity, "bubble": null, "name": "Engineering Hall"},
-// 	ELH:  {"latlng": L.latLng(33.64447, -117.84071), "distance": Infinity, "bubble": null, "name": "Engineering Lecture Hall"},
-// 	HG:   {"latlng": L.latLng(33.64825, -117.84451), "distance": Infinity, "bubble": null, "name": "Humanities Gateway"},
-// 	HH:   {"latlng": L.latLng(33.64733, -117.84398), "distance": Infinity, "bubble": null, "name": "Humanities Hall"},
-// 	HIB:  {"latlng": L.latLng(33.64841, -117.84383), "distance": Infinity, "bubble": null, "name": "Humanities Instructional Building"},
-// 	HICF: {"latlng": L.latLng(33.64687, -117.84680), "distance": Infinity, "bubble": null, "name": "Humanities Interim Classroom Facility"},
-// 	HSLH: {"latlng": L.latLng(33.64559, -117.84466), "distance": Infinity, "bubble": null, "name": "Howard Schneiderman Lecture Hall"},
-// 	IAB:  {"latlng": L.latLng(33.64822, -117.84534), "distance": Infinity, "bubble": null, "name": "Intercollegiate Athletics Building"},
-// 	ICF:  {"latlng": L.latLng(33.64443, -117.83997), "distance": Infinity, "bubble": null, "name": "Interim Classroom Facility"},
-// 	ICS:  {"latlng": L.latLng(33.64428, -117.84180), "distance": Infinity, "bubble": null, "name": "Information & Computer Science"},
-// 	LLIB: {"latlng": L.latLng(33.64713, -117.84109), "distance": Infinity, "bubble": null, "name": "Langson Library"},
-// 	MM:   {"latlng": L.latLng(33.64933, -117.84445), "distance": Infinity, "bubble": null, "name": "Music & Media Building"},
-// 	MPAA: {"latlng": L.latLng(33.64707, -117.83695), "distance": Infinity, "bubble": null, "name": "Multipurpose Academic & Administrative Building"},
-// 	MSTB: {"latlng": L.latLng(33.64208, -117.84442), "distance": Infinity, "bubble": null, "name": "Multipurpose Science & Technology Building"},
-// 	PCB:  {"latlng": L.latLng(33.64451, -117.84281), "distance": Infinity, "bubble": null, "name": "Parkview Classroom Building"},
-// 	PSCB: {"latlng": L.latLng(33.64341, -117.84349), "distance": Infinity, "bubble": null, "name": "Physical Sciences Classroom Building"},
-// 	PSLH: {"latlng": L.latLng(33.64340, -117.84396), "distance": Infinity, "bubble": null, "name": "Physical Sciences Lecture Hall"},
-// 	RH:   {"latlng": L.latLng(33.64451, -117.84412), "distance": Infinity, "bubble": null, "name": "Rowland Hall"},
-// 	SBSG: {"latlng": L.latLng(33.64737, -117.83909), "distance": Infinity, "bubble": null, "name": "Social & Behavioral Sciences Gateway"},
-// 	SE:   {"latlng": L.latLng(33.64615, -117.83889), "distance": Infinity, "bubble": null, "name": "Social Ecology I"},
-// 	SE2:  {"latlng": L.latLng(33.64656, -117.83915), "distance": Infinity, "bubble": null, "name": "Social Ecology II"},
-// 	SH:   {"latlng": L.latLng(33.64621, -117.84484), "distance": Infinity, "bubble": null, "name": "Steinhaus Hall"},
-// 	SSH:  {"latlng": L.latLng(33.64623, -117.84007), "distance": Infinity, "bubble": null, "name": "Social Science Hall"},
-// 	SSL:  {"latlng": L.latLng(33.64590, -117.84000), "distance": Infinity, "bubble": null, "name": "Social Science Lab"},
-// 	SSLH: {"latlng": L.latLng(33.64723, -117.83973), "distance": Infinity, "bubble": null, "name": "Social Science Lecture Hall"},
-// 	SSPA: {"latlng": L.latLng(33.64694, -117.83955), "distance": Infinity, "bubble": null, "name": "Social Science Plaza A"},
-// 	SST:  {"latlng": L.latLng(33.64646, -117.84027), "distance": Infinity, "bubble": null, "name": "Social Science Tower"},
-// 	SSTR: {"latlng": L.latLng(33.64698, -117.84024), "distance": Infinity, "bubble": null, "name": "Social Science Trailer"}
+// 	BS3:  {"latlng": L.latLng(33.64532, -117.84608), "distance": Infinity, "watcherBubble": null, "name": "Biological Sciences III"},
+// 	DBH:  {"latlng": L.latLng(33.64326, -117.84198), "distance": Infinity, "watcherBubble": null, "name": "Donald Bren Hall"},
+// 	EH:   {"latlng": L.latLng(33.64376, -117.84101), "distance": Infinity, "watcherBubble": null, "name": "Engineering Hall"},
+// 	ELH:  {"latlng": L.latLng(33.64447, -117.84071), "distance": Infinity, "watcherBubble": null, "name": "Engineering Lecture Hall"},
+// 	HG:   {"latlng": L.latLng(33.64825, -117.84451), "distance": Infinity, "watcherBubble": null, "name": "Humanities Gateway"},
+// 	HH:   {"latlng": L.latLng(33.64733, -117.84398), "distance": Infinity, "watcherBubble": null, "name": "Humanities Hall"},
+// 	HIB:  {"latlng": L.latLng(33.64841, -117.84383), "distance": Infinity, "watcherBubble": null, "name": "Humanities Instructional Building"},
+// 	HICF: {"latlng": L.latLng(33.64687, -117.84680), "distance": Infinity, "watcherBubble": null, "name": "Humanities Interim Classroom Facility"},
+// 	HSLH: {"latlng": L.latLng(33.64559, -117.84466), "distance": Infinity, "watcherBubble": null, "name": "Howard Schneiderman Lecture Hall"},
+// 	IAB:  {"latlng": L.latLng(33.64822, -117.84534), "distance": Infinity, "watcherBubble": null, "name": "Intercollegiate Athletics Building"},
+// 	ICF:  {"latlng": L.latLng(33.64443, -117.83997), "distance": Infinity, "watcherBubble": null, "name": "Interim Classroom Facility"},
+// 	ICS:  {"latlng": L.latLng(33.64428, -117.84180), "distance": Infinity, "watcherBubble": null, "name": "Information & Computer Science"},
+// 	LLIB: {"latlng": L.latLng(33.64713, -117.84109), "distance": Infinity, "watcherBubble": null, "name": "Langson Library"},
+// 	MM:   {"latlng": L.latLng(33.64933, -117.84445), "distance": Infinity, "watcherBubble": null, "name": "Music & Media Building"},
+// 	MPAA: {"latlng": L.latLng(33.64707, -117.83695), "distance": Infinity, "watcherBubble": null, "name": "Multipurpose Academic & Administrative Building"},
+// 	MSTB: {"latlng": L.latLng(33.64208, -117.84442), "distance": Infinity, "watcherBubble": null, "name": "Multipurpose Science & Technology Building"},
+// 	PCB:  {"latlng": L.latLng(33.64451, -117.84281), "distance": Infinity, "watcherBubble": null, "name": "Parkview Classroom Building"},
+// 	PSCB: {"latlng": L.latLng(33.64341, -117.84349), "distance": Infinity, "watcherBubble": null, "name": "Physical Sciences Classroom Building"},
+// 	PSLH: {"latlng": L.latLng(33.64340, -117.84396), "distance": Infinity, "watcherBubble": null, "name": "Physical Sciences Lecture Hall"},
+// 	RH:   {"latlng": L.latLng(33.64451, -117.84412), "distance": Infinity, "watcherBubble": null, "name": "Rowland Hall"},
+// 	SBSG: {"latlng": L.latLng(33.64737, -117.83909), "distance": Infinity, "watcherBubble": null, "name": "Social & Behavioral Sciences Gateway"},
+// 	SE:   {"latlng": L.latLng(33.64615, -117.83889), "distance": Infinity, "watcherBubble": null, "name": "Social Ecology I"},
+// 	SE2:  {"latlng": L.latLng(33.64656, -117.83915), "distance": Infinity, "watcherBubble": null, "name": "Social Ecology II"},
+// 	SH:   {"latlng": L.latLng(33.64621, -117.84484), "distance": Infinity, "watcherBubble": null, "name": "Steinhaus Hall"},
+// 	SSH:  {"latlng": L.latLng(33.64623, -117.84007), "distance": Infinity, "watcherBubble": null, "name": "Social Science Hall"},
+// 	SSL:  {"latlng": L.latLng(33.64590, -117.84000), "distance": Infinity, "watcherBubble": null, "name": "Social Science Lab"},
+// 	SSLH: {"latlng": L.latLng(33.64723, -117.83973), "distance": Infinity, "watcherBubble": null, "name": "Social Science Lecture Hall"},
+// 	SSPA: {"latlng": L.latLng(33.64694, -117.83955), "distance": Infinity, "watcherBubble": null, "name": "Social Science Plaza A"},
+// 	SST:  {"latlng": L.latLng(33.64646, -117.84027), "distance": Infinity, "watcherBubble": null, "name": "Social Science Tower"},
+// 	SSTR: {"latlng": L.latLng(33.64698, -117.84024), "distance": Infinity, "watcherBubble": null, "name": "Social Science Trailer"}
 // };
 
 /* Leaflet */
@@ -48,45 +48,49 @@ var tiles = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?ac
 });
 tiles.addTo(leafletmap);
 
-var bubble = L.circle([90, 180]);
-bubble.addTo(leafletmap);
+var watcherBubble = L.circle([90, 180]);
+watcherBubble.addTo(leafletmap);
 
-var mmm = L.marker([90, 180]);
-mmm.on("click", function() {
-	leafletmap.setView(mmm.getLatLng(), 16);
+var watcherMarker = L.marker([90, 180]);
+watcherMarker.on("click", function() {
+	leafletmap.setView(watcherMarker.getLatLng(), 16);
 });
-mmm.addTo(leafletmap);
+watcherMarker.addTo(leafletmap);
 
-geo.filter(e => e.marker_feed.includes("academic")).forEach(function(bldg) {
+geo.filter(e => e.name.includes("(")).forEach(function(bldg) {
 	bldg.latlng = L.latLng(bldg.lat, bldg.lng);
-	bldg.bubble = L.circle(bldg.latlng, 15, {
+	bldg.watcherBubble = L.circle(bldg.latlng, 15, {
 		"weight": 1,
 		"color": "#0064a4",
 		"fillOpacity": 0,
 		"renderer": myRenderer
 	});
-	bldg.bubble.bindPopup(bldg.name);
-	bldg.bubble.addTo(leafletmap);
+	bldg.watcherBubble.bindPopup(bldg.name);
+	bldg.watcherBubble.addTo(leafletmap);
 });
 
 function onLeafletLocate(location) {
-	bubble.setLatLng(location.latlng);
-	bubble.setRadius(location.accuracy);
-	bubble.setStyle({
+	watcherBubble.setLatLng(location.latlng);
+	watcherBubble.setRadius(location.accuracy);
+	watcherBubble.setStyle({
 		"stroke": false,
 		"color": "#6aa2b8",
 		"opacity": 0.75
 	});
-	bubble.redraw();
+	watcherBubble.redraw();
 
-	mmm.setLatLng(location.latlng);
+	watcherMarker.setLatLng(location.latlng);
 
-	geo.filter(e => e.marker_feed.includes("academic")).forEach(function(bldg) {
+	geo.filter(e => e.name.includes("(")).forEach(function(bldg) {
 		bldg.distance = leafletmap.distance(location.latlng, bldg.latlng);
-		bldg.bubble.setStyle({
+		bldg.watcherBubble.setStyle({
 			"fillOpacity": 1 / Math.pow(Math.E, bldg.distance / 272)
 		});
+
+        database.filter(e => bldg.name.includes(e.id.split(" ")[0]))
+        .forEach(e => e.distance = bldg.distance);
 	});
+    //timetable.sortByProperty("distance");
 }
 
 function onLeafletLocateError(e) {
@@ -99,6 +103,9 @@ watcher.on("locationerror", onLeafletLocateError);
 
 /* Table */
 
+for(var x, i = 0; x = database[i]; i++) {
+    x.id = "<a href=\"https://www.reg.uci.edu/perl/WebSoc?YearTerm=2017-92&ShowFinals=0&ShowComments=0&Days=" + "UMTWHFS"[new Date().getDay()] + "&Bldg=" + x.id.split(" ")[0] + "&Room=" + x.id.split(" ")[1] + "\" target=\"_blank\">" + x.id + "</a>";
+}
 var timetable = TimeTable.new(database, ["room", "distance", "last_active"]);
 document.getElementById("list").appendChild(timetable.table);
 
