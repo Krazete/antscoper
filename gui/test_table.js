a1d588dd766818e1b94a14dbbe44267d9f945009
Ted Michael Celis <krazete@gmail.com>
2018-10-21 01:55:55 -0700

    still reworking gui
    basic js is mostly complete

--- gui/test_table.js
+++ gui/test_table.js
@@ -1,7 +1,7 @@
 var day = 1; // 0 = sunday
 
-var scroller = document.getElementById("scroller");
-var dynamicStyle = document.getElementById("dynamic-style");
+var scroller;
+var dynamicStyle;
 
 function init() {
     initDays();
@@ -26,43 +26,72 @@ function toggle() { // TODO: delete this crap and put in an actual day switcher
 }
 
 function initTimeline() {
+    scroller = document.getElementById("scroller");
+    dynamicStyle = document.getElementById("dynamic-style");
     for (var building in database) {
-        var buildingTitle = createBuildingTitle(building);
-        var buildingBlock = createBuildingBlock(building);
-        scroller.appendChild(buildingTitle);
-        scroller.appendChild(buildingBlock);
+        scroller.appendChild(createBuildingTitle(building));
+        scroller.appendChild(createBuildingBlock(building));
     }
 }
 
 function createBuildingTitle(building) {
     var buildingTitle = document.createElement("div");
-        buildingTitle.className = "building-title";
+        buildingTitle.className = "building";
         buildingTitle.innerHTML = building.toUpperCase();
     return buildingTitle;
 }
 
 function createBuildingBlock(building) {
-    var buildingBlock = document.createElement("div");
-        buildingBlock.className = "building-block";
-        var timeline = createTimeline();
-        buildingBlock.appendChild(timeline);
-        for (var room of database[building]) {
-            var roomBlock = createRoomBlock();
-            buildingBlock.appendChild(roomBlock);
-        }
-    return buildingBlock;
+    var buildingTable = document.createElement("div");
+        buildingTable.className = "timetable";
+        buildingTable.dataset.building = building; // TODO: weigh this vs createBuildingTitle
+        buildingTable.appendChild(createTimeline(building));
+        buildingTable.appendChild(createSchedule(building));
+    return buildingTable;
 }
 
-function createTimeline() {
-    var timelines = document.createElement("div");
-        timelines.className = "timelines";
-        var timeline = document.createElement("div");
-            timeline.className = "timeline";
-            var timeblock = document.createElement("div");
-                timeblock.className = "timeblock";
-            timeline.appendChild(timeblock);
-        timelines.appendChild(timeline);
-    return timelines;
+function createTimeline(building) {
+    var timeline = document.createElement("div");
+        timeline.className = "timeline";
+        timeline.dataset.building = building.toUpperCase();
+        for (var i = 0; i < 24; i++) {
+            timeline.appendChild(createTimeBlock(i));
+        }
+    return timeline;
+}
+
+function createTimeBlock(i) {
+    var timeblock = document.createElement("div");
+        timeblock.className = "timeblock";
+        timeblock.innerHTML = (i % 12 ? i : 12) + (i < 12 ? "am" : "pm");
+    return timeblock;
+}
+
+function createSchedule(building) {
+    var scheduleSet = document.createElement("div");
+        scheduleSet.className = "scheduleNOT";
+        for (var room in database[building]) {
+            scheduleSet.appendChild(createRoomBlock(building, room));
+        }
+    return scheduleSet;
+}
+
+function createRoomBlock(building, room) {
+    var schedule = document.createElement("div");
+        schedule.className = "schedule";
+        schedule.dataset.room = room.toUpperCase();
+        for (var hours of database[building][room].schedule[day]) {
+            schedule.appendChild(createTimeBubble(hours[0], hours[1]));
+        }
+    return schedule;
+}
+
+function createTimeBubble(a, b) {
+    var timebubble = document.createElement("div");
+        timebubble.className = "timebubble";
+        timebubble.style.left = (100 * a / 24) + "%";
+        timebubble.style.width = (100 * (b - a) / 24) + "%";
+    return timebubble;
 }
 
 function updateTime() {
