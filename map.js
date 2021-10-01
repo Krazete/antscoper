ddc11d11ab0fb9253c3e8504cfb0264a040d79e6
Ted Michael Celis <krazete@gmail.com>
2021-10-01 03:31:09 -0700

    fixed mapbox tile api

--- map.js
+++ map.js
@@ -8,10 +8,11 @@ function initMap() {
     var myRenderer = L.svg({
         "padding": 2
     });
-    var tiles = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
+    var tiles = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         "maxZoom": 18,
-        "id": "mapbox.streets",
-        "worldCopyJump": true
+        "id": "mapbox/streets-v11",
+        "worldCopyJump": true,
+        "accessToken": "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
     });
     tiles.addTo(leafletmap);
 
